7b15cd239fada01507edfb3ddd9177f7510b4f65
David <david.sanchez@tecnologicoliceocristiano.edu.ec>
2024-01-13 08:17:13 -0500

    Agregar archivos de proyecto.

---
+++ TareaAPI/Controllers/FootbalPlayerController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TareaAPI.Models;
+
+namespace TareaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootbalPlayerController : ControllerBase
+    {
+        private static List<FootbalPlayer> _players = new List<FootbalPlayer>
+        {
+            new FootbalPlayer
+            {
+                ID = 1,
+                FullName = "Cristiano Ronaldo",
+                Team = "Al-Nassr",
+                Goals = "873"
+            },
+            new FootbalPlayer
+            {
+                ID = 2,
+                FullName = "Lionel Messi",
+                Team = "Inter Miami CF",
+                Goals = "800"
+            },
+            new FootbalPlayer
+            {
+                ID = 3,
+                FullName = "Robert Lewandowski",
+                Team = "Fútbol Club Barcelona",
+                Goals = "500"
+            },
+            new FootbalPlayer
+            {
+                ID = 4,
+                FullName = "Kylian Mbappé",
+                Team = "París Saint-Germán",
+                Goals = "310"
+            }
+        };
+
+        [HttpGet]
+        public IEnumerable<FootbalPlayer> GetAllPlayers()
+        {
+            return _players;
+        }
+    }
+}
---
+++ TareaAPI/Models/FootbalPlayer.cs
@@ -0,0 +1,11 @@
+﻿namespace TareaAPI.Models
+{
+    public class FootbalPlayer
+    {
+        //Propiedades o atributos
+        public int ID { get; set; }
+        public string FullName { get; set; }
+        public string Team { get; set; }
+        public string Goals { get; set; }
+    }
+}
